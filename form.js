4fd8f9f49e4eed10573f3a2adaffbfa5a4f5c0c5
Agripaw <krista@agripaw.com>
2025-10-25 08:03:35 -0700

    Update form.js

--- form.js
+++ form.js
@@ -1,1 +1,11 @@
+// Handle dropdown toggle open/close
+document.addEventListener("click", function (e) {
+  const isDropdownButton = e.target.matches(".dropbtn");
+  const dropdown = e.target.closest(".dropdown");
 
+  if (isDropdownButton && dropdown) {
+    dropdown.classList.toggle("open");
+  } else {
+    document.querySelectorAll(".dropdown.open").forEach((d) => d.classList.remove("open"));
+  }
+});
